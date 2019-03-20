324f757c1147c499b7b5e853832dfe30b28e263a
Bao Pham <ryanvuepham@gmail.com>
2019-03-19 21:33:12 -0500

    migration created for students & seeding

---
+++ data/seeds/001-cohorts.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('cohorts').truncate()  // change table_name to roles. remove .del() and replaces with .truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('cohorts').insert([
+        {name: 'student'},
+        {name: 'PM'},
+        {name: 'TA'}
+      ])
+    })
+}
